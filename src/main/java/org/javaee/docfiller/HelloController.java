89fbb18cf3c30d40db1ce18fce28ea70a6f5ed83
Oleksandr Karpenko <1ooksapp8@gmail.com>
2025-05-11 13:45:24 +0300

    Initial commit. Made main page with all buttons

---
+++ src/main/java/org/javaee/docfiller/HelloController.java
@@ -0,0 +1,14 @@
+package org.javaee.docfiller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to Documentaries filler Application!");
+    }
+}
